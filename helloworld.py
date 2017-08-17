19ec5283074082d9459ffa83b2d37260a77b7ef0
Madhav Ramkumar <madhav.ramkumar@gmail.com>
2017-08-17 17:26:54 -0400

    code complete house price prediction model

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,14 @@
+# import TensorFlow
 import tensorflow as tf
-hello = tf.constant('Hello, Tensorflow!')
+
 sess = tf.Session()
+
+# Verify we can print a string
+hello = tf.constant('Hello, Tensorflow!')
 print(sess.run(hello))
+
+#Perform some simple math
+a = tf.constant(20)
+b = tf.constant(10)
+print('a + b = {0}'.format(sess.run(a + b)))
+
